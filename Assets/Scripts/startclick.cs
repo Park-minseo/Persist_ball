45c898a573bb6bbf6a1aed12bd9a362b9f89ac45
ZeiveR <jangmur41@gmail.com>
2024-05-19 23:54:49 +0900

    achievement window
    도전과제 창 업데이트

--- Assets/Scripts/startclick.cs
+++ Assets/Scripts/startclick.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class startclick : MonoBehaviour
 {
+
+    public ScrollRect achieve;
 
     public void goto_Game()
     {
@@ -18,6 +22,15 @@ public class startclick : MonoBehaviour
     public void goto_Setting()
     {
         SceneManager.LoadScene("Setting_room");
+    }
+
+
+    public void goto_achi()
+    {
+        if (achieve != null)
+        {
+            achieve.gameObject.SetActive(!achieve.gameObject.activeSelf);
+        }
     }
     // Start is called before the first frame update
     void Start()
